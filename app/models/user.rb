2cfee8c5555cd5eceb7859299d00bae02d136913
shushpan <pavelshushpan@gmail.com>
2017-01-20 20:10:19 +0300

    add func. when first user is admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
+  before_save do
+    self.admin = true if User.count.zero?
+  end
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true, length: {minimum: 2}
   validates :email, presence: true, length: {minimum: 4}
+
 end

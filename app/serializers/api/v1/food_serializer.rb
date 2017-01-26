feb97ae5eb6ca97bfb4763383e71a8ec97348e67
shushpan <pavelshushpan@gmail.com>
2017-01-26 19:36:20 +0300

    remove food serializer, fixed order serializer

--- app/serializers/api/v1/food_serializer.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::FoodSerializer < ActiveModel::Serializer
-  attribute :food_type
-  attribute :name
-  attribute :price
-  attribute :photo
-end
--- app/serializers/api/v1/order_serializer.rb
+++ app/serializers/api/v1/order_serializer.rb
@@ -1,7 +1,20 @@
 class Api::V1::OrderSerializer < ActiveModel::Serializer
-  belongs_to :user, serializer: Api::V1::UserSerializer
-
-  has_many :foods, serializer: FoodSerializer do
-    Food.where(date: Date.today)
+  belongs_to :user, serializer: UserSerializer
+  attribute :id
+  attribute :first_course_id, key: :first_course do
+    get_food_by_id(object.first_course_id)
+  end
+  attribute :main_course_id, key: :main_course do
+    get_food_by_id(object.main_course_id)
+  end
+  attribute :drink_id, key: :drink do
+    get_food_by_id(object.drink_id)
+  end
+  attribute :total_amount
+  def get_food_by_id id
+    unless id.zero?
+     food = Food.find(id)
+      {name: food.name, photo: food.photo.url, price: food.price}
     end
+  end
 end

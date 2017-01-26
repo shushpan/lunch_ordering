a89709b39ac693f76575b3f14848a174bf712d8f
shushpan <pavelshushpan@gmail.com>
2017-01-26 17:37:24 +0300

    fixed result output food type in orders list

--- app/assets/javascripts/food-order.js
+++ app/assets/javascripts/food-order.js
@@ -13,6 +13,14 @@ function set_total_amount() {
     $('#total-amount').html(total_amount + " &#36")
 
 }
+
+function check_undefined_id(el) {
+    if(el == undefined)
+        return 0
+    else
+        return el
+}
+
 $('.menu-wrap .food').on('click', function () {
     var wrapper = '.' + $(this).parent().parent().attr('class') + ' .food'
     set_radio_active_flag(wrapper,$(this),'food_active')
@@ -24,14 +32,14 @@ $('#order-btn').on('click', function(){
         main_course = $('.main-course-wrap').find('.food_active').attr('data-id'),
         drink =  $('.drink-wrap').find('.food_active').attr('data-id')
 
-    if(first_course!=undefined && main_course!=undefined  && drink!=undefined ){
+    if(first_course!=undefined || main_course!=undefined  || drink!=undefined ){
         var req = $.ajax({
             url: '/new_order',
             type: 'post',
             data:{
-                'first_course_id': first_course,
-                'main_course_id': main_course,
-                'drink_id': drink
+                'first_course_id': check_undefined_id(first_course),
+                'main_course_id': check_undefined_id(main_course),
+                'drink_id': check_undefined_id(drink)
             },
             dataType: 'text',
             success: function(){
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
 
-  def new
+  def create
     order = Order.new({user_id: current_user.id, date: Date.today, first_course_id: params[:first_course_id],
                       main_course_id: params[:main_course_id], drink_id: params[:drink_id]})
     order.calculate_total_amount

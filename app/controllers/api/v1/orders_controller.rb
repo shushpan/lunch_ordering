25fce689904282a20a185881b5eb23ab6ebe3776
shushpan <pavelshushpan@gmail.com>
2017-01-26 23:24:34 +0300

    add result output orders by api

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-
+    orders = Order.where(date: Date.today)
+    render json: orders, status: 200
   end
 end
